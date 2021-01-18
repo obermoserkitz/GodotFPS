9b322ddb69067b9b8382d138a027d66499cdb8fc
Bernhard Obermoser <obermoser.kitz@gmail.com>
2021-01-18 09:52:12 +0100

    Blah

---
+++ scripts/Player.gd
@@ -0,0 +1,11 @@
+extends KinematicBody
+const GRAVITY = -24.8
+var vel = Vector3()
+
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
